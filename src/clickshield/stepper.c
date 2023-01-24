e95e76ce73adf09b30e8aae5c8e5c69ddc627024
XI <xi@xivps.xyz>
2023-01-24 17:46:45 +0100

    Get stepper actually working

--- src/clickshield/stepper.c
+++ src/clickshield/stepper.c
@@ -16,12 +16,39 @@
 #define STEPEN_PORT GPIOA
 #define STEPEN_PIN LL_GPIO_PIN_3
 
+static volatile enum {
+  RUN_IDLE=0,
+  RUN_LATCH,
+  RUN_END
+} control_status;
+
 static int32_t lastpos = 0;
+static int32_t newpos = 0;
+static volatile int32_t relpos = 0;
 
 void TIM1_UP_TIM16_IRQHandler() {
   if (LL_TIM_IsActiveFlag_UPDATE(TIM1)) {
     LL_TIM_ClearFlag_UPDATE(TIM1);
-    LL_GPIO_TogglePin(STEPSD_PORT, STEPDIR_PIN);
+    if (control_status == RUN_LATCH) {
+      // make next move
+    } else {
+      control_status = RUN_IDLE;
+    }
+  }
+}
+
+void TIM1_CC_IRQHandler() {
+  if (LL_TIM_IsActiveFlag_CC3(TIM1)) {
+    LL_TIM_ClearFlag_CC3(TIM3);
+    if (relpos > ((uint32_t)1<<16)-1) {
+      relpos -= 0xFFFF;
+      control_status = RUN_LATCH;
+      LL_TIM_SetRepetitionCounter(TIM1, (uint16_t)0xFFFF-1);
+    } else {
+      LL_TIM_SetRepetitionCounter(TIM1, (uint16_t)relpos);
+      LL_TIM_SetOnePulseMode(TIM1, LL_TIM_ONEPULSEMODE_SINGLE);
+      control_status = RUN_END;
+    }
   }
 }
 
@@ -31,17 +58,16 @@ void cs_step_init() {
   LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOB);
   LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOC);
 
+  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_TIM1);
+
   LL_GPIO_InitTypeDef s_gpioinit;
   LL_GPIO_StructInit(&s_gpioinit);
   s_gpioinit.Pin = STEPSTEP_PIN;
   s_gpioinit.Mode = LL_GPIO_MODE_ALTERNATE;
-  //s_gpioinit.Pull = LL_GPIO_PULL_UP;
-  //s_gpioinit.Mode = LL_GPIO_MODE_INPUT;
+  //s_gpioinit.Mode = LL_GPIO_MODE_OUTPUT;
   s_gpioinit.Speed = LL_GPIO_SPEED_HIGH;
   s_gpioinit.Alternate = LL_GPIO_AF_1; // TIM1
   LL_GPIO_Init(STEPSD_PORT, &s_gpioinit);
-
-  //s_gpioinit.Pull = LL_GPIO_PULL_NO;
 
   s_gpioinit.Pin = STEPDIR_PIN;
   s_gpioinit.Mode = LL_GPIO_MODE_OUTPUT;
@@ -60,23 +86,18 @@ void cs_step_init() {
   s_gpioinit.Pin = STEPEN_PIN;
   LL_GPIO_Init(STEPEN_PORT, &s_gpioinit);
 
-  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_TIM1);
-
   LL_TIM_InitTypeDef s_timinit;
   LL_TIM_StructInit(&s_timinit);
-  s_timinit.Prescaler = 800; // with ARRL=1000 frequency of 800Hz
-  s_timinit.Autoreload = 1000;
+  s_timinit.Prescaler = 8000; // 100Hz pwm frequency
+  s_timinit.Autoreload = 100;
   s_timinit.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
-  //s_timinit.RepetitionCounter = 0; //is set afterwards
+  s_timinit.RepetitionCounter = 0; // is set afterwards
   LL_TIM_Init(TIM1, &s_timinit);
-
-  LL_TIM_CC_DisablePreload(TIM1);
 
   LL_TIM_OC_InitTypeDef s_timocinit;
   LL_TIM_OC_StructInit(&s_timocinit);
   s_timocinit.CompareValue = s_timinit.Autoreload/2;
-  //s_timocinit.OCMode = LL_TIM_OCMODE_ACTIVE;
-  s_timocinit.OCMode = LL_TIM_OCMODE_PWM1;
+  s_timocinit.OCMode = LL_TIM_OCMODE_PWM2;
   s_timocinit.OCNState = LL_TIM_OCSTATE_ENABLE;
   s_timocinit.OCNPolarity = LL_TIM_OCPOLARITY_HIGH;
   s_timocinit.OCNIdleState = LL_TIM_OCIDLESTATE_LOW;
@@ -86,8 +107,8 @@ void cs_step_init() {
   NVIC_EnableIRQ(TIM1_UP_TIM16_IRQn);
   NVIC_SetPriority(TIM1_UP_TIM16_IRQn, 0x01);
 
+  LL_TIM_EnableAllOutputs(TIM1);
   LL_GPIO_SetOutputPin(STEPEN_PORT, STEPEN_PIN);
-  LL_TIM_EnableCounter(TIM1);
 }
 
 void cs_step_setmode(cs_step_mode_t mode) {
@@ -104,16 +125,28 @@ void cs_step_setmode(cs_step_mode_t mode) {
 }
 
 int cs_step_move(int32_t pos) {
-  int32_t relpos = pos - lastpos;
+  if (LL_TIM_IsEnabledCounter(TIM1)) {
+    return 1; // Error as move is currently active
+  }
+  // calc position increment
+  relpos = pos - newpos;
+  newpos = pos + relpos;
   if (relpos < 0) {
     LL_GPIO_ResetOutputPin(STEPSD_PORT, STEPDIR_PIN);
   } else {
     LL_GPIO_SetOutputPin(STEPSD_PORT, STEPDIR_PIN);
   }
-  // calc position increment
-  // write values
-  //LL_TIM_SetRepetitionCounter(TIM1, (uint32_t)relpos);
-  //LL_TIM_SetOnePulseMode(TIM1, LL_TIM_ONEPULSEMODE_SINGLE);
-  //LL_TIM_EnableCounter(TIM1);
-  // enable timer
+  relpos = (uint32_t)relpos; // make value positive
+  if (relpos > ((uint32_t)1<<16)-1) {
+    relpos -= 0xFFFF;
+    control_status = RUN_LATCH;
+    LL_TIM_SetRepetitionCounter(TIM1, (uint16_t)0xFFFF-1);
+  } else {
+    LL_TIM_SetRepetitionCounter(TIM1, (uint16_t)relpos);
+    LL_TIM_SetOnePulseMode(TIM1, LL_TIM_ONEPULSEMODE_SINGLE);
+    control_status = RUN_END;
+  }
+  LL_TIM_EnableCounter(TIM1);
+
+  return 0;
 }
--- src/main.c
+++ src/main.c
@@ -18,15 +18,6 @@
 #include <string.h>
 #include <stdio.h>
 
-static CS_RGB_TypeDef rgbcolor = {255,0,0};
-static CS_RGB_TypeDef patterns[] = {
-  {0,0,0},
-  {255,0,0},
-  {0,255,0},
-  {0,0,255},
-  {255,255,255},
-};
-static const uint8_t patterncnt = sizeof(patterns)/sizeof(CS_RGB_TypeDef);
 static bool tim_elapsed = false;
 static unsigned int btncnt = 0;
 
@@ -34,22 +25,17 @@ static void controlloop(uint32_t looptime);
 static void btnhandler(CS_BTN_Action_TypeDef value);
 
 static uint8_t serhelp(char * outbuf, char * const cmdbuf __attribute__((unused)));
-static uint8_t seradd(char * outbuf, char * const cmdbuf);
-static uint8_t ser_measadc(char * outbuf, char * const cmdbuf);
-static uint8_t ser_getbtnpresses(char * outbuf, char * const cmdbuf __attribute__((unused)));
 
 /**
  * @brief control loop callback for os tick
  * @params looptime contains the tick count value
  */
 static void controlloop(uint32_t looptime) {
-  //LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_3);
-  //cs_rot_handle();
-  //uint16_t leds = cs_rot_calcIndicator(cs_rot_getPos()%16,16);
-  //cs_rot_setIndicator(leds);
+  cs_rot_handle();
+  uint16_t leds = cs_rot_calcIndicator(cs_rot_getPos()%30,30);
+  cs_rot_setIndicator(leds);
   CS_LoopHandler(looptime);
   ser_handle();
-  //LL_GPIO_SetOutputPin(GPIOB, LL_GPIO_PIN_3);
 }
 
 /**
@@ -75,92 +61,23 @@ static uint8_t serhelp(char * outbuf, char * const cmdbuf __attribute__((unused)
   return 0;
 }
 
-/*
- * @brief serial function which adds 2 numbers
- * @note both serial arguments must be non-zero
- */
-static uint8_t seradd(char * outbuf, char * const cmdbuf) {
-  int a=0,b=0;
-  a = atoi(cmdbuf);
-  char * const blocation = strchr(cmdbuf,',');
-  if (blocation != NULL) {
-    b = atoi(blocation+1);
-  } else {
-    snprintf(outbuf, SER_MAX_RESPLEN, "Missing Argument error");
-    return 1;
-  }
-  if (a!=0 && b!=0) {
-    snprintf(outbuf, SER_MAX_RESPLEN, "result: %d", a+b);
-  } else {
-    snprintf(outbuf, SER_MAX_RESPLEN, "Argument error");
-    return 1;
-  }
-  return 0;
-}
-
-/*
- * @brief serial function which sets a dac value(arg 1) and prints the measured adc value
- */
-static uint8_t ser_measadc(char * outbuf, char * const cmdbuf) {
-  if (strlen(cmdbuf) < 1) {
-    snprintf(outbuf, SER_MAX_RESPLEN, "Missing Argument error");
-    return 1;
-  }
-  uint16_t ret = 0;
-  uint16_t dacval = atoi(cmdbuf);
-  ret += mydac_setval(dacval, MYADC_PIN_DAC1);
-  uint16_t adcval = 100;
-  ret += myadc_getval(&adcval);
-  snprintf(outbuf, SER_MAX_RESPLEN, "ADC: %d", adcval);
-  return ret;
-}
-
-/*
- * @brief serial function which prints the button fast presses since last long press
- */
-static uint8_t ser_getbtnpresses(char * outbuf, char * const cmdbuf __attribute__((unused))) {
-  snprintf(outbuf, SER_MAX_RESPLEN, "Button pressed %u times.", btncnt);
-  return 0;
-}
-
-/**
- * @brief callback to demonstrate the os_timeout functionality
- */
-void timeouthandler() {
-  tim_elapsed = true;
-}
-
 int main()
 {
-  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOB);
-  //LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_3, LL_GPIO_MODE_OUTPUT);
-  //LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_3);
-
-  CS_Init(CS_INIT_BTN | CS_INIT_RGB | CS_INIT_DIM);
   CS_Init(CS_INIT_BTN);
   CS_BTN_SetCallback(btnhandler);
 
-  myadc_configure(MYADC_PIN_DAC1);
-
   ser_init();
   ser_addcmd('h', serhelp);
-  ser_addcmd('a', seradd);
-  ser_addcmd('m', ser_measadc);
-  ser_addcmd('b', ser_getbtnpresses);
 
-  //cs_rot_init();
-  //cs_step_init();
+  cs_rot_init();
+  cs_step_init();
+  cs_step_setmode(CS_STEP_FULL);
 
   // enable event loop
   os_setcallback(controlloop);
 
-  // wait for 3 seconds after board reset
-  //os_timeout(3e9, timeouthandler);
-  //while (!tim_elapsed);
-
-  uint8_t pat = 0;
 	while (1) {
-    //cs_step_move(-10000);
+    cs_step_move(10);
     os_timeout(250e6, NULL);
     /*
     ser_buf_TypeDef * buffer = ser_get_free_buf();
